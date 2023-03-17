b3c8e40ce5d4a208b96bade816d71dfaa4ac96e8
olusola ogungbeje <oraphealgbeje@gmail.com>
2023-03-17 13:45:01 -0700

    new putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main (void)
+/**
+ * This displays a string
+ */
 {
-	puts ("\"Programming is like building a multillingual puzzle");
-	return (0);
+puts ("Programming is like building a multillingual puzzle");
+return (0);
 }
