fc9cddeba2b1b34025de842689794f278ef2610d
olusola ogungbeje <oraphealgbeje@gmail.com>
2023-03-20 15:43:26 -0700

    combo

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point to print numbers between 0 to 9 and letters btw a to f
+ *
+ * Return: always returns value 0 (success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 103; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - displays all possible combinations of single-digit numbers
+ * Return: always return 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
